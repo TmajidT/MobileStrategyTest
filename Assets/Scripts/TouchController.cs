ef1dd909c405057bf05d82306c7ff548587e0168
Majid Asfanani <majid.asfanani@gmail.com>
2026-08-14 12:04:04 +0330

    added input for mobile

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -7,16 +7,34 @@ public class TouchController : MonoBehaviour
 
     void Update()
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        // Touch
+        if (Touchscreen.current != null)
+        {
+            var touch = Touchscreen.current.primaryTouch;
+
+            if (touch.press.wasPressedThisFrame)
+            {
+                Vector2 touchPosition = touch.position.ReadValue();
+                HandleInput(touchPosition);
+            }
+        }
+
+        // Mouse
+        if (Mouse.current != null &&
+            Mouse.current.leftButton.wasPressedThisFrame)
         {
             Vector2 mousePosition = Mouse.current.position.ReadValue();
+            HandleInput(mousePosition);
+        }
+    }
 
-            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+    private void HandleInput(Vector2 screenPosition)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
 
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                Instantiate(cubePrefab, hit.point, Quaternion.identity);
-            }
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Instantiate(cubePrefab, hit.point, Quaternion.identity);
         }
     }
 }
