c77e4d5e065c23e78d6293f55e0d1b1da1b31e77
Majid Asfanani <majid.asfanani@gmail.com>
2026-08-14 12:19:50 +0330

    added drag

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -3,30 +3,106 @@ using UnityEngine.InputSystem;
 
 public class TouchController : MonoBehaviour
 {
+    [SerializeField] private float cameraDragSpeed = 0.02f;
+    [SerializeField] private float dragThreshold = 20f;
+
+    private Vector2 startInputPosition;
+    private Vector2 previousInputPosition;
+
+    private bool isDragging;
+
     void Update()
     {
-        // Touch
         if (Touchscreen.current != null)
         {
             var touch = Touchscreen.current.primaryTouch;
 
             if (touch.press.wasPressedThisFrame)
             {
-                Vector2 touchPosition = touch.position.ReadValue();
-                HandleInput(touchPosition);
+                startInputPosition = touch.position.ReadValue();
+                previousInputPosition = startInputPosition;
+
+                isDragging = false;
+            }
+
+            if (touch.press.isPressed)
+            {
+                Vector2 currentPosition = touch.position.ReadValue();
+
+                Vector2 totalDelta = currentPosition - startInputPosition;
+
+                if (!isDragging &&
+                    totalDelta.magnitude > dragThreshold)
+                {
+                    isDragging = true;
+                }
+
+                if (isDragging)
+                {
+                    Vector2 delta = currentPosition - previousInputPosition;
+
+                    MoveCamera(delta);
+
+                    previousInputPosition = currentPosition;
+                }
+            }
+
+            if (touch.press.wasReleasedThisFrame)
+            {
+                if (!isDragging)
+                {
+                    HandleTap(touch.position.ReadValue());
+                }
+
+                isDragging = false;
             }
         }
 
-        // Mouse
-        if (Mouse.current != null &&
-            Mouse.current.leftButton.wasPressedThisFrame)
+        if (Mouse.current != null)
         {
-            Vector2 mousePosition = Mouse.current.position.ReadValue();
-            HandleInput(mousePosition);
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                startInputPosition = Mouse.current.position.ReadValue();
+                previousInputPosition = startInputPosition;
+
+                isDragging = false;
+            }
+
+            if (Mouse.current.leftButton.isPressed)
+            {
+                Vector2 currentPosition = Mouse.current.position.ReadValue();
+
+                Vector2 totalDelta = currentPosition - startInputPosition;
+
+                if (!isDragging &&
+                    totalDelta.magnitude > dragThreshold)
+                {
+                    isDragging = true;
+                }
+
+                if (isDragging)
+                {
+                    Vector2 delta = currentPosition - previousInputPosition;
+
+                    MoveCamera(delta);
+
+                    previousInputPosition = currentPosition;
+                }
+            }
+
+            if (Mouse.current.leftButton.wasReleasedThisFrame)
+            {
+                if (!isDragging)
+                {
+                    HandleTap(Mouse.current.position.ReadValue());
+                }
+
+                isDragging = false;
+            }
         }
     }
 
-    private void HandleInput(Vector2 screenPosition)
+    private void HandleTap(Vector2 screenPosition)
     {
         Ray ray = Camera.main.ScreenPointToRay(screenPosition);
 
@@ -39,5 +115,17 @@ public class TouchController : MonoBehaviour
                 territory.Select();
             }
         }
+    }
+
+    private void MoveCamera(Vector2 delta)
+    {
+        Vector3 movement = new Vector3(
+            -delta.x,
+            0,
+            -delta.y
+        );
+
+        Camera.main.transform.position +=
+            movement * cameraDragSpeed;
     }
 }
